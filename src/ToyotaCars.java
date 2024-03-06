25fb6a4e5d652f4fb4f99f2fcc0088617cbdc5fb
bibasss <119476848+bibasss@users.noreply.github.com>
2024-03-06 13:25:30 +0500

    Its first comit!

--- src/ToyotaCars.java
+++ src/ToyotaCars.java
@@ -10,14 +10,14 @@ public abstract class ToyotaCars {
     }
 
     public void processed(){
-        System.out.println(description+" is processed");
+        System.out.println(description+" is processed...");
     }
 
     public void assembled(){
-        System.out.println(description+ " is assembled");
+        System.out.println(description+ " is assembled...");
     }
     public void packed(){
-        System.out.println(description+" is packed");
+        System.out.println(description+" is packed...");
     }
     public void ready(){
         System.out.println(description+" ready! \n");
